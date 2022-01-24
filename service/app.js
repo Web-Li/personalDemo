4f83d579cd4044ec137d1ad354b1bfea32b39692
liyatao7 <liyatao7@jd.com>
2022-01-24 19:04:59 +0800

    feat nginx

--- service/app.js
+++ service/app.js
@@ -16,6 +16,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('json spaces', 2);
 
 app.use(logger('dev'));
 app.use(express.json());
